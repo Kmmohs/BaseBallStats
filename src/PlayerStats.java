34176ac33f2da4d7ddd34bf4543d2f99bab661b3
Kenny <Kenny@128.4.108.165>
2018-09-18 16:25:10 -0400

    2 intial commit

--- src/PlayerStats.java
+++ src/PlayerStats.java
@@ -5,15 +5,16 @@ public class PlayerStats {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
+		String playerName;
 		int atBats;
-		System.out.println("")
 		int hits;
 		int doubles;
 		int triples;
 		int homeRuns;
 		int runs;
 		int walks;
-
+		
+		System.out.println("Player Name: " + input.nextLine());)
 	}
 
 }
