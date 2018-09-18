2035177ae7ab27070907c506b497390fb0a69f6c
Kenny <Kenny@128.4.108.165>
2018-09-18 16:19:58 -0400

    Quiz 1

---
+++ src/PlayerStats.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PlayerStats {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int atBats;
+		System.out.println("")
+		int hits;
+		int doubles;
+		int triples;
+		int homeRuns;
+		int runs;
+		int walks;
+
+	}
+
+}
