616e9a6d672aa9f07e704ad6399d1ccaf3ab9e13
Kenny <Kenny@128.4.108.165>
2018-09-18 16:52:03 -0400

    Updated no methods

--- src/PlayerStats.java
+++ src/PlayerStats.java
@@ -5,16 +5,57 @@ public class PlayerStats {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		String playerName;
-		int atBats;
-		int hits;
-		int doubles;
-		int triples;
-		int homeRuns;
-		int runs;
-		int walks;
+		System.out.println("Player Name: ");
+		String playerName = input.nextLine();
 		
-		System.out.println("Player Name: " + input.nextLine());)
+		System.out.println("At Bat: ");
+		int atBats = input.nextInt();
+		
+		System.out.println("Number of Hits: ");
+		int hits = input.nextInt();
+		
+		System.out.println("Number of Doubles: ");
+		int doubleBases = input.nextInt();
+		
+		System.out.println("Number of Triples: ");
+		int tripleBases = input.nextInt();
+		
+		System.out.println("Number of Home Runs: ");
+		int homeRuns = input.nextInt();
+		
+		System.out.println("Number of Runs: ");
+		int runs = input.nextInt();
+		
+		System.out.println("Number of Walks: ");
+		int walks = input.nextInt();
+		
+		System.out.println(BattingAverage(atBats, hits));
+		
 	}
-
+	
+	public static double BattingAverage(int atBats, int hits) {
+		double battingAverage = (double)hits/(double)atBats;
+		return battingAverage;
+		
+	}
+	
+	public double OnBasePercentage() {
+		return onBasePercentage;
+		
+	}
+	
+	public double SluggingPercentage() {
+		return sluggingPercentage;
+		
+	}
+	
+	public double OBS() {
+		onBase + sluggingPercentage
+		return obs
+	}
+	
+	public int TotalBases() {
+		runs * 4 + walks
+	}
+	
 }
